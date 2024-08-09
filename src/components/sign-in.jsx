ef6be0f520490cbf8c913e9c5940c4519717032e
Rhythm1821 <alamrawat1821@gmail.com>
2024-08-10 01:01:46 +0530

    signin and signout btn

--- src/components/sign-in.jsx
+++ src/components/sign-in.jsx
@@ -1,15 +1,16 @@
 import { signIn } from "@/auth";
 
 export default function SignIn() {
+    
     return (
-        <form
+        <form className="flex flex-col gap-4 text-white border w-1/5 bg-slate-400 hover:bg-slate-500 border-white"
          action={
             async ()=>{
                 "use server"
-                await signIn("google")
+                await signIn()
             }
          }>
-            <button>Sign In with Google</button>
+            <button>Sign In</button>
         </form>
     );
 }
